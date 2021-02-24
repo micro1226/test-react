72d3f2e616516a60511e1f9716977585b9fc0fbd
Mary.Qian <maryqian@augmentum.com.cn>
2021-02-24 14:09:57 +0800

    setup base

---
+++ src/components/baseUse/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import JSXBaseDemo from './JSXBaseDemo'
+
+class BaseUseDemo extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div>
+            {/* <JSXBaseDemo /> */}
+            123
+        </div>
+    }
+}
+
+export default BaseUseDemo
